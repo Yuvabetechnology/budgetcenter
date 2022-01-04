837cd9168d9325b5998d20bd385ea8f08955c3ed
MariyaAnsi <mariya.ansi1998@gmail.com>
2022-01-04 11:55:22 +0530

    formula,auto-fetch data and jinja templating added

--- budgetcenter/budgetcenter/doctype/maintenance_master_list/maintenance_master_list.js
+++ budgetcenter/budgetcenter/doctype/maintenance_master_list/maintenance_master_list.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, Yuvavbe and contributors
+// Copyright (c) 2022, Yuvavbe and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('Maintenance Master List', {
