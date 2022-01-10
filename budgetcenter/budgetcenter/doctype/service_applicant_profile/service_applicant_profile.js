69df5296ee9ce82380b263467371885713013c73
MariyaAnsi <mariya.ansi1998@gmail.com>
2022-01-10 18:14:17 +0530

    updated in narrative information form and added applicant profile doctype

--- budgetcenter/budgetcenter/doctype/service_applicant_profile/service_applicant_profile.js
+++ budgetcenter/budgetcenter/doctype/service_applicant_profile/service_applicant_profile.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2022, Yuvavbe and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Recurring Expenses Item', {
+frappe.ui.form.on('Service Applicant Profile', {
 	// refresh: function(frm) {
 
 	// }
--- budgetcenter/budgetcenter/doctype/service_applicant_profile/service_applicant_profile.py
+++ budgetcenter/budgetcenter/doctype/service_applicant_profile/service_applicant_profile.py
@@ -4,5 +4,5 @@
 # import frappe
 from frappe.model.document import Document
 
-class RecurringExpensesItem(Document):
+class ServiceApplicantProfile(Document):
 	pass
